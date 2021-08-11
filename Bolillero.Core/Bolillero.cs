f976e8d955176d51e6c5734763843ba4e5d05ec2
Mauricioagreda <mauricio.agreda.d2@gmail.com>
2021-08-11 16:08:24 -0300

    comit03

--- Bolillero.Core/Bolillero.cs
+++ Bolillero.Core/Bolillero.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Bolillero.Core
 {
@@ -13,6 +15,49 @@ namespace Bolillero.Core
         //   .RemoveAT(num)
         //   Clear()
 
+    byte cantidad {get; set;}
+    byte indiceAzar {get; set;}
+
+    List<byte> Afuera {get; set;}
+    List<byte> Adentro {get; set;}
+    Random r;
+
+    public Bolillero()
+    {
+        Afuera = new List<byte>{};
+        Afuera = new List<byte>{};
+        r = new Random();
+    }
+
+    public Bolillero(byte cantidad)
+    {
+        this.Llenar(this.cantidad);
+    }
+
+    private void Llenar(byte cantidad)
+    {
+        for(byte i=0; i < cantidad; i++)
+        {
+            Adentro.Add(i);
+        }
+    }
+
+    public byte SacarBolilla()
+    {
+        byte indiceAzar =(byte)r.Next(0, Adentro.Count);
+        byte bolilla = Adentro[indiceAzar];
+        Adentro.RemoveAt(indiceAzar);
+        Afuera.Add(bolilla);
+        
+        return bolilla;
+    }
+
+    public void RellenarBolillero()
+    {
+        Adentro.AddRange(Afuera);
+        Afuera.Clear();
+    }
+    
     public Jugada(byte[] jugadas)
 	{
 		private byte[] jugada;
@@ -23,13 +68,7 @@ namespace Bolillero.Core
         }
 	}
 
-    public SacarBolilla()
-    {
-        if ()
-        {
-            
-        }
-    }
+   
 
 	public IngresarBolilla()
 
