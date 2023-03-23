1481db592076a1974e7f09ff31eafa2d01d740c9
Mauricioagreda <mauricio.agreda.d2@gmail.com>
2023-03-23 15:16:29 -0300

    Update(-Bolillero-)
    Metodos:
    -SacarBolilla();
    -ReIngresar(List<Bolilla>);

--- SimulacionBolillero/Bolillero.cs
+++ SimulacionBolillero/Bolillero.cs
@@ -9,19 +9,48 @@ namespace SimulacionBolillero
     {
         public int Id {get; set;}
         public List<Bolilla> bolillas {get; set;}
+
+        private const int V = 0;
+        List<Bolilla> listBolillas = new List<Bolilla>();
         int wins;
-        public Bolillero()
-        { }
         Random rnd = new Random();
+
+        public Bolillero()
+        { 
+            for (int i = 1; i == 10; i++)
+            {
+                bolillas.Add(new Bolilla(i));
+            }
+        }
+        
         public void SacarBolilla()
         {
-            Bolilla bolilla = new Bolilla(){Numero = rnd.Next(0, 50)};
-            bolillas.Add(bolilla);
+            int num = rnd.Next(1,10);
+            var bolilla = bolillas.Where(x => x.Numero == num).ToList();
+            bolillas.RemoveAll(x => x.Numero == num);
+            if (bolillas.Count == 9) 
+            {
+                listBolillas.AddRange(bolilla);
+                ReIngresar(bolilla);
+            }
+            else
+            {
+                throw new Exception("Error al sacar bolilla");
+            }
+            bolilla.Clear();
+        }
+        public void ReIngresar(List<Bolilla> x)
+        {
+            bolillas.AddRange(x);
+            if(bolillas.Count(x => x.Numero > 0) < 10 )
+            {
+                throw new Exception("Bolillas fuera del bolillero");
+            }
         }
         public void Jugada (List<Bolilla> a, int jugadas)
         {
             wins = 0;
-            for (int f = 0; f == jugadas; f++)
+            for (var f = 0; f == jugadas; f++)
             {
                 Console.WriteLine("-----------------------");
                 for (int i = 0; i == 9; i++)
@@ -30,14 +59,15 @@ namespace SimulacionBolillero
                 }
                 if (a == bolillas)
                 {
-                    wins++;
                     Console.WriteLine("Mamamguevo");
                 }
                 else 
                 {
+                    wins++;
                     Console.WriteLine("Digo glugluglu");
                 }
                 bolillas.Clear();
+                listBolillas.Clear();
             }
             Console.WriteLine($"Cantidad Aciertos: {wins}");
         }
